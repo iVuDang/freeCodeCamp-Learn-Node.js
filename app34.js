8f6f8a6c7a9a88f1f871c22915176bd6de7314af
iVuDang <vdang159@mtroyal.ca>
2021-05-09 16:31:53 -0600

    40 min mark

--- app34.js
+++ app34.js
@@ -6,9 +6,25 @@ fs.writeFile('example.txt',"this is an example",(err)=>{
         console.log(err);
     else 
         console.log('File successfully created');
+        fs.readFile('example.txt','utf8',(err,file)=>{
+            if(err)
+                console.log(err);
+            else
+                console.log(file);
+        })
 }); 
 
 
 /*
-    * Created new file called 'example.txt' in our folder.
+    * Created new file called 'example.txt' in our folder, inside the file has the text 'this is an example
+    
+    * fs.writeFile() method replaces the specified file and content if it exists. If the file does not exist, a new file, containing the specified content, will be created
+        * syntax: name of file, contents of file, call back function
+    
+    * fs.readFile() method is used to read files on your computer.
+        * syntax: name of file, encoding, call back function
+        
+    * Highlighting all, and 'contol + /' will comment out the highlighted coding lines
+
+
 */

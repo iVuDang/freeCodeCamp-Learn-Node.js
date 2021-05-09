0483ead4b0c7251c1ffb7c7db44fed56035117cb
iVuDang <vdang159@mtroyal.ca>
2021-05-08 22:59:08 -0600

    18 min

--- app14.js
+++ app14.js
@@ -2,11 +2,14 @@ const EventEmitter = require('events');
 const eventEmitter = new EventEmitter(); 
 
 
-eventEmitter.on('tutorial', ()=>{
-    console.log('tutorial event has occurred'); 
+eventEmitter.on('tutorial', (num1,num2)=>{
+    console.log(num1 + num2); 
 });
+// parameters should not have spaces otherwise NaN result
 
 
-eventEmitter.emit('tutorial');
+eventEmitter.emit('tutorial',1,2);
+// node app14 => 3
+
 // .emit is a method
 // Emit's job is to trigger named event(s) which in turn cause functions called listeners to be called

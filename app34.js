399fd910a456353620e3a3b459666ed80d8878a0
iVuDang <vdang159@mtroyal.ca>
2021-05-09 17:09:40 -0600

    40 min mark

--- app34.js
+++ app34.js
@@ -1,18 +1,18 @@
 const fs = require('fs');
 // create a file
 
-fs.writeFile('example.txt',"this is an example",(err)=>{
-    if(err)
-        console.log(err);
-    else 
-        console.log('File successfully created');
-        fs.readFile('example.txt','utf8',(err,file)=>{
-            if(err)
-                console.log(err);
-            else
-                console.log(file);
-        })
-}); 
+// fs.writeFile('example.txt',"this is an example",(err)=>{
+//     if(err)
+//         console.log(err);
+//     else 
+//         console.log('File successfully created');
+//         fs.readFile('example.txt','utf8',(err,file)=>{
+//             if(err)
+//                 console.log(err);
+//             else
+//                 console.log(file);
+//         })
+// }); 
 
 
 /*
@@ -28,3 +28,10 @@ fs.writeFile('example.txt',"this is an example",(err)=>{
 
 
 */
+
+fs.rename('example.txt','newFileName.txt',(err)=>{
+    if(err)
+        console.log(err);
+    else
+        console.log('successfully renamed the file');
+});

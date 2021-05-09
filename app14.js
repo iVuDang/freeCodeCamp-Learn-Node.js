21d6d23ca5568f79acb268e0933a5b02805f3bdb
iVuDang <vdang159@mtroyal.ca>
2021-05-08 23:38:15 -0600

    20 min mark

--- app14.js
+++ app14.js
@@ -13,3 +13,32 @@ eventEmitter.emit('tutorial',1,2);
 
 // .emit is a method
 // Emit's job is to trigger named event(s) which in turn cause functions called listeners to be called
+
+
+class Person extends EventEmitter{
+    constructor(name){
+        super();
+        this._name = name;
+    }
+
+    get name(){
+        return this._name;
+    }
+}
+
+// The constructor() method is a special method for creating and initializing objects created within a class.
+// The super keyword refers to the parent class. It is used to call the constructor of the parent class and to access the parent's properties and methods.
+
+// Extends keyword is used in class declarations or class expressions to create a class that is a child of another class.
+
+let iVuDang = new Person('iVuDang');
+
+iVuDang.on('name',()=>{
+    console.log('my name is ' + iVuDang.name);
+});
+// The on() method attaches one or more event handlers for the selected elements and child elements.
+
+
+iVuDang.emit('name');
+// node app14 => my name is iVuDang
+

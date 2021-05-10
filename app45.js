85544ba83c3c6da2a8acc0ef009d388c9e23056e
iVuDang <vdang159@mtroyal.ca>
2021-05-09 20:32:54 -0600

    53 min mark

--- app45.js
+++ app45.js
@@ -1,15 +1,22 @@
 const fs = require ('fs');
+
 fs.mkdir('tutorialFolder',(err)=>{
-    if(err){
+    if(err)
         console.log(err);
-    }else{
-        console.log('folder successfully created');
-    }
-})
+    else
+        fs.writeFile('./tutorialFolder/example.txt','data inside file',(err)=>{
+            if(err)
+                console.log(err);
+            else
+                console.log('successfully created file')
+            })
+        })
+    
 
 
 
 /*
     * mkdir = make directory or folder
-
+    * 
+    * 
 */

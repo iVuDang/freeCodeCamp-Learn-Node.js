7980827e47bf47f3e131f3cbf5b0dc5980203b57
iVuDang <vdang159@mtroyal.ca>
2021-05-09 20:47:19 -0600

    57:36 finished File system module pt 2 -  creating and deleting folders

--- app45.js
+++ app45.js
@@ -1,22 +1,51 @@
+const { ifError } = require('assert');
 const fs = require ('fs');
 
-fs.mkdir('tutorialFolder',(err)=>{
-    if(err)
-        console.log(err);
-    else
-        fs.writeFile('./tutorialFolder/example.txt','data inside file',(err)=>{
-            if(err)
-                console.log(err);
-            else
-                console.log('successfully created file')
-            })
-        })
+// fs.mkdir('tutorialFolder',(err)=>{
+//     if(err)
+//         console.log(err);
+//     else
+//         fs.writeFile('./tutorialFolder/example.txt','data inside file',(err)=>{
+//             if(err)
+//                 console.log(err);
+//             else
+//                 console.log('successfully created file')
+//             })
+//         })
     
-
-
-
 /*
     * mkdir = make directory or folder
     * 
     * 
 */
+
+// fs.unlink('./tutorialFolder/example.txt',(err)=>{
+//     if(err)
+//         console.log(err);
+//     else 
+//         fs.rmdir('tutorialFolder',(err)=>{
+//             if(err)
+//                 console.log(err);
+//             else 
+//                 console.log('deleted folder');
+//         })
+// })
+
+
+fs.readdir('example',(err,files)=>{
+    if(err)
+        console.log(err);
+    else{
+        for(let file of files){
+            fs.unlink('./example/' + file,(err)=>{
+                if(err)
+                    console.log(err);
+                else{
+                    console.log('successfully deleted');
+                }
+            })
+        }
+    }
+});
+
+// Deletes files a.txt and b.txt in our folder 'example' 
